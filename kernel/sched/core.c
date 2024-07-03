eee0b82d51e1baad00474ffc77cfb3c1e366b8a6
Yang Yingliang <yangyingliang@huawei.com>
2024-07-03 11:16:10 +0800

    sched/core: Fix unbalance set_rq_online/offline() in sched_cpu_deactivate()
    If cpuset_cpu_inactive() fails, set_rq_online() need be called to rollback.
    
    Fixes: 120455c514f7 ("sched: Fix hotplug vs CPU bandwidth control")
    Cc: stable@kernel.org
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240703031610.587047-5-yangyingliang@huaweicloud.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8149,6 +8149,7 @@ int sched_cpu_deactivate(unsigned int cpu)
 
 	ret = cpuset_cpu_inactive(cpu);
 	if (ret) {
+		sched_set_rq_online(rq, cpu);
 		balance_push_set(cpu, false);
 		set_cpu_active(cpu, true);
 		return ret;
