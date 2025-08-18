84e7feb747f9f113dff2d46f7f124cbff72e35c2
EmanuelCN <emanuelghub@gmail.com>
2025-08-18 15:41:30 +0300

    !fixup: sched: Use cpu_dying() to fix balance_push vs hotplug-rollback

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7959,7 +7959,6 @@ static void balance_push_set(int cpu, bool on)
 	struct rq_flags rf;
 
 	rq_lock_irqsave(rq, &rf);
-	rq->balance_push = on;
 	if (on) {
 		WARN_ON_ONCE(rq->balance_callback);
 		rq->balance_callback = &balance_push_callback;
